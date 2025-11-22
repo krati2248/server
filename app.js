bd4f4d4c28dc218e3bcdf5d9d803a4638d56420a
krati2248 <184264500+krati2248@users.noreply.github.com>
2025-11-22 19:30:33 +0530

    production

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(fileUpload({
 app.use(cookieParser());
 // Use this
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://client-u2ci.vercel.app',
   credentials: true 
 }));
 
